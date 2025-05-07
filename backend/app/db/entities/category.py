988c63c33dc9c9bb328183fbabebf77a77dbd13f
JBLOZ <jordiblloz@gmail.com>
2025-05-07 17:24:41 +0200

    feat(db): añadir clase Category para la representación de entidades en SQLAlchemy

--- backend/app/db/entities/category.py
+++ backend/app/db/entities/category.py
@@ -1,6 +1,7 @@
 """ Category entity representation for SQLAlchemy ORM. """
 
 from enum import Enum
+from sqlmodel import SQLModel, Field
 
 
 class CategoryTypes(Enum):
@@ -11,3 +12,8 @@ class CategoryTypes(Enum):
     ZAPATOS = 4
     PORTATILES = 5
     OTROS = 6
+
+
+class Category(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    name: str
