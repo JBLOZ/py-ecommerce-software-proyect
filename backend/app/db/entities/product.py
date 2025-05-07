890cf24d07d5bc5d5ac7b932b5098a13c5137c2b
JBLOZ <jordiblloz@gmail.com>
2025-05-07 17:26:54 +0200

    feat(db): definir la clase Product para la representación de productos en SQLAlchemy

--- backend/app/db/entities/product.py
+++ backend/app/db/entities/product.py
@@ -1,1 +1,12 @@
 """ Product entity representation for SQLAlchemy ORM. """
+
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class Product(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    name: str
+    description: Optional[str] = None
+    price: float
+    category_id: int = Field(foreign_key="category.id")
